c79a0a6caea871fdcfd83272c2b76de2942adb12
sachithra-manamperi <manamper-se18028@stu.kln.ac.lk>
2021-09-27 18:47:54 +0530

    Admin Login

---
+++ src/adminlogin/Main.java
@@ -0,0 +1,21 @@
+package adminlogin;
+import java.util.Scanner;  // Import the Scanner class
+
+
+
+public class Main {
+    Scanner uInput = new Scanner(System.in);  // Create a Scanner object
+    System.out.println("Enter username");
+
+    String userName = uInput.nextLine();  // Read user input
+    System.out.println("Username is: " + userName);  // Output user input
+
+    System.out.println("Enter password");
+
+    String password= uInput.nextLine();
+
+
+
+
+
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+import java.util.Scanner;  // Import the Scanner class
+
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //admin Login
+
+
+
+
+    }
+
+
+}
