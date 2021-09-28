fe5154ce0f4fb913af78768dbbb82add3c139dd2
Gayan Malinda <73253202+GayanMalinda99@users.noreply.github.com>
2021-09-29 03:22:01 +0530

    create main

---
+++ src/main/java/com/kelaniya/uni/g5/Main.java
@@ -0,0 +1,11 @@
+package main.java.com.kelaniya.uni.g5;
+
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        System.out.println("Hello");
+    }
+
+    }
