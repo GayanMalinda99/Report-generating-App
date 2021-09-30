667225d6735f5bd9ced4b7cff17b88101677eb94
Tharaka Ariyarathna <69495064+tharaka-ariyarathna@users.noreply.github.com>
2021-09-30 20:15:55 +0530

    Added code for send attachment
    Mailgun

--- Report-generating-App/src/main/java/com/report_generating_app/uni/Emails.java
+++ Report-generating-App/src/main/java/com/report_generating_app/uni/Emails.java
@@ -5,18 +5,21 @@ import kong.unirest.JsonNode;
 import kong.unirest.Unirest;
 import kong.unirest.UnirestException;
 
+import java.io.File;
+
 public class Emails {
 
     private static final String YOUR_DOMAIN_NAME = "sandboxda9c4b996ef341898279b43c284bd6c0.mailgun.org";
     private static final String API_KEY = "4ac88df3a5f251caaf471f91c1e5d18a-dbdfb8ff-8ca2f010" ;
 
-    public static JsonNode sendSimpleMessage() throws UnirestException {
+    public  JsonNode sendSimpleMessage() throws UnirestException {
         HttpResponse<JsonNode> request = Unirest.post("https://api.mailgun.net/v3/" + YOUR_DOMAIN_NAME + "/messages")
-			.basicAuth("api", API_KEY)
+                .basicAuth("api", API_KEY)
                 .queryString("from", "softwareconstructionteam5@gmail.com")
-                .queryString("to", "tharakaariyarathna04@gmail.com")
-                .queryString("subject", "Test")
-                .queryString("text", "Works fine")
+                .queryString("to", "alexw3253@gmail.com")
+                .queryString("subject", "hello")
+                .queryString("text", "It worked")
+                .field("attachment", new File("Report.xlsx"))
                 .asJson();
         return request.getBody();
     }
--- Report-generating-App/src/main/java/com/report_generating_app/uni/Main.java
+++ Report-generating-App/src/main/java/com/report_generating_app/uni/Main.java
@@ -2,11 +2,15 @@ package com.report_generating_app.uni;
 
 import kong.unirest.JsonNode;
 
+
+import java.io.File;
+
 public class Main {
     public static void main(String[] args) {
         Emails email = new Emails() ;
-        JsonNode jsonNode =  email.sendSimpleMessage() ;
+        JsonNode jsonNode = email.sendSimpleMessage() ;
         System.out.println(jsonNode.toString());
     }
+
 
 }
