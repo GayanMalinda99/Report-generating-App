d0a8bffee8c809acc4f00d0c376114c41193dc30
Gayan Malinda <73253202+GayanMalinda99@users.noreply.github.com>
2021-10-02 09:34:18 +0530

    Delete ReportTypeAndDurationInputs.java

--- src/main/java/com/kelaniya/uni/g5/inputs/ReportTypeAndDurationInputs.java
+++
@@ -1,8 +0,0 @@
-package com.kelaniya.uni.g5.inputs;
-
-public interface ReportTypeAndDurationInputs {
-    String getReportType() throws InvalidInputException;
-    String getStartingDate() throws InvalidInputException;
-    String getEndingDate() throws InvalidInputException;
-    String getStoringAndSendingType() throws InvalidInputException;
-}
