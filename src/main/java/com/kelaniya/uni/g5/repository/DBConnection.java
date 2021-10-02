999557699d73409a361f360b5e70ec49781760c4
sachithra-manamperi <manamper-se18028@stu.kln.ac.lk>
2021-10-02 22:49:11 +0530

    Adding Files

---
+++ src/main/java/com/kelaniya/uni/g5/repository/DBConnection.java
@@ -0,0 +1,21 @@
+package com.kelaniya.uni.g5.repository;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBConnection {
+    public Connection getConnection()
+    {
+        Connection connection=null;
+        System.out.println("Connection Called");
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection= DriverManager.getConnection("jdbc:mysql://localhost:3306/university","root","");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return connection;
+    }
+}
+
