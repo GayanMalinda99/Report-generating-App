1b161167de8a8d2b274f5cd22d779f85cbd6f5b1
Umayanga Vidunuwan <73218580+Vidunuwan@users.noreply.github.com>
2021-10-02 19:24:49 +0530

    Add files via upload

---
+++ GetEndDate.java
@@ -0,0 +1,22 @@
+package com.kelaniya.uni.v1;
+
+public class GetEndDate {
+    private final String[] args;
+
+    public GetEndDate(String[] arguments) {
+        this.args = arguments;
+    }
+
+    public String getEndDate(){
+
+        String endDate = args[2];
+        //YMD
+        String type ="^[\\d]{4}-[\\d]{2}-[\\d]{2}$";
+
+        if (!endDate.matches(type)){
+            System.out.println("Please provide the correct end date as argument");
+            return "";
+        }
+        return endDate;
+    }
+}
