3d107bf79a1da3f044e13d0d0aeafa652976b12c
Issiwara Kavinda <issiwaraperera9912@gmail.com>
2021-10-03 01:06:21 +0530

    Genarate Detailed

--- src/main/java/com/kelaniya/uni/g5/Main.java
+++ src/main/java/com/kelaniya/uni/g5/Main.java
@@ -1,24 +1,30 @@
 package com.kelaniya.uni.g5;
 
+import com.kelaniya.uni.g5.inputs.IO.*;
 import com.kelaniya.uni.g5.inputs.Inputs;
 import com.kelaniya.uni.g5.inputs.InvalidInputException;
 import com.kelaniya.uni.g5.inputs.UserInputs;
+import com.kelaniya.uni.g5.reports.DetailedReport;
 import com.kelaniya.uni.g5.reports.OperationFactory;
+import com.kelaniya.uni.g5.reports.SummaryReport;
 
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException, InvalidInputException {
 
-        Inputs inputs = new UserInputs();
 
-        String reportType = inputs.getReportType();
-        String startingDate = inputs.getStartingDate();
-        String endingDate = inputs.getEndingDate();
-        String sendingType = inputs.getSendingType();
+        GetReportType reportType = new GetReportType(args);
+        GetStartDate startDate = new GetStartDate(args);
+        GetEndDate endDate = new GetEndDate(args);
+        GetRequestMethod requestMethod = new GetRequestMethod(args);
+        GetEmail getEmail = new GetEmail(args);
+        DetailedReport detailedReport=new DetailedReport();
 
-        OperationFactory operationFactory = new OperationFactory();
-        operationFactory.getInstance(reportType);
+        
+        detailedReport.execute(startDate.getStartDate(),endDate.getEndDate());
+
+
 
 
 
